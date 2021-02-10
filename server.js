4583b28daeafddbd6c903611d12a0fda9d78ee8c
Minju <kimmin47@naver.com>
2021-02-10 17:18:56 +0900

    Add Get Customeers REST API Module

--- server.js
+++ server.js
@@ -5,8 +5,30 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-res.send({ message: 'Hello Express!' });
+app.get('/api/customers', (req, res) => {
+res.send([
+    {
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/any',
+        'name': '박지민',
+        'gender' : '남',
+        'job': '가수'
+        },
+        {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/any',
+        'name': '박지민',
+        'gender' : '남',
+        'job': '가수'
+        },
+        {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/any',
+        'name': '박지민',
+        'gender' : '남',
+        'job': '가수'
+        }  
+]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
